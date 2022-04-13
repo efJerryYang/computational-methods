2a3f4f44e3348141a964d5d5bfd936f37c27333d
Jerry Yang <efJerryYang@outlook.com>
2022-04-13 11:08:23 +0800

    feat: Demo binary file
    But can not execute on another machine, this bug still fails to fix

---
+++ hitsz/Demo/src/run_lab_demo.jl
@@ -0,0 +1,8 @@
+# using Plots
+# p = plot(rand(2,2))
+# display(p)
+include("lab1-lagrange.jl")
+include("lab2-romberg.jl")
+include("lab3-rungekutta.jl")
+include("lab4-newton.jl")
+include("lab5-gauss.jl")
