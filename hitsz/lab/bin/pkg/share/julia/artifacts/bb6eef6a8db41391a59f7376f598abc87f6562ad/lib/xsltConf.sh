45031f0763fac9cc0443ffdc2ca692d714c95536
Jerry Yang <efJerryYang@outlook.com>
2022-04-26 14:50:18 +0800

    chore: remove binary files from bin directory

--- hitsz/lab/bin/pkg/share/julia/artifacts/bb6eef6a8db41391a59f7376f598abc87f6562ad/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/workspace/destdir/lib"
-XSLT_LIBS="-lxslt -L/workspace/destdir/lib -lxml2  "
-XSLT_PRIVATE_LIBS=""
-XSLT_INCLUDEDIR="-I/workspace/destdir/include"
-MODULE_VERSION="xslt-1.1.34"
